CloudburstMC/Cloudburst

commit 076effab56f1067f592a219fca06059d2920133e

Fix gamemode bug (#858)

server/src/main/java/cn/nukkit/Server.java
@@ -1318,7 +1318,11 @@ public class Server {
     }
 
     public int getGamemode() {
-        return this.getPropertyInt("gamemode", 0) & 0b11;
+        try {
+            return this.getPropertyInt("gamemode", 0) & 0b11;
+        } catch (NumberFormatException exception) {
+            return getGamemodeFromString(this.getPropertyString("gamemode")) & 0b11;
+        }
     }
 
     public boolean getForceGamemode() {
@@ -1367,7 +1371,7 @@ public class Server {
             case "v":
                 return Player.SPECTATOR;
         }
-        return -1;
+        return Player.SURVIVAL;
     }
 
     public static int getDifficultyFromString(String str) {
@@ -1423,7 +1427,7 @@ public class Server {
 
     public int getDefaultGamemode() {
         if (this.defaultGamemode == Integer.MAX_VALUE) {
-            this.defaultGamemode = this.getPropertyInt("gamemode", 0);
+            this.defaultGamemode = this.getGamemode();
         }
         return this.defaultGamemode;
     }

commit 0cd27d5b6e3d3e5b475e6dfd6357468a20a7e7c2

Fix bug introduced by #858 It should be -1 because it's used in commands to determine if the input is valid.

server/src/main/java/cn/nukkit/Server.java
@@ -1371,7 +1371,7 @@ public class Server {
             case "v":
                 return Player.SPECTATOR;
         }
-        return Player.SURVIVAL;
+        return -1;
     }
 
     public static int getDifficultyFromString(String str) {