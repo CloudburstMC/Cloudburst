42c1b6e4f8b1caa5e22131ce0ea18f8ef5b417d8
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2020-08-06 20:43:12 +0100

    Block state unit test now works correct

--- src/test/java/org/cloudburstmc/server/registry/BlockRegistryTest.java
+++ src/test/java/org/cloudburstmc/server/registry/BlockRegistryTest.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.server.registry;
 
+import com.nukkitx.blockstateupdater.BlockStateUpdaters;
 import com.nukkitx.nbt.NBTInputStream;
 import com.nukkitx.nbt.NbtList;
 import com.nukkitx.nbt.NbtMap;
@@ -10,14 +11,19 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BlockRegistryTest {
 
     @DisplayName("Block Palette Test")
     @Test
-    public void blockPaletteTest() {
+    public void blockPaletteTest() throws IOException {
         Set<NbtMap> vanillaPalette;
         InputStream stream = BlockRegistryTest.class.getClassLoader().getResourceAsStream("runtime_block_states.dat");
         try (NBTInputStream nbtStream = NbtUtils.createNetworkReader(stream)) {
@@ -27,21 +33,37 @@ public class BlockRegistryTest {
             throw new AssertionError("Unable to load block palette");
         }
 
+        int version = BlockStateUpdaters.getLatestVersion();
+        int major = (version >> 24) & 0xFF;
+        int minor = (version >> 16) & 0xFF;
+        int patch = (version >> 8) & 0xFF;
+        int build = version & 0xFF;
+        System.out.printf("Latest block state version: %d.%d.%d.%d%n", major, minor, patch, build);
+
         NbtList<NbtMap> serverPalette = BlockRegistry.get().getPaletteTag(); // init
 
-        boolean failed = false;
+        List<String> invalidStates = new ArrayList<>();
+        int invalid = 0;
         for (NbtMap state : serverPalette) {
             if (!vanillaPalette.remove(state)) {
-                System.out.println("State does not exist in vanilla palette:\n" + state.toString());
-                failed = true;
+                invalidStates.add(state.toString());
+                invalid++;
             }
         }
 
+        System.out.println("Found " + invalid + " invalid block states");
+        Files.write(Paths.get("invalid_states.log"), invalidStates, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+
+        List<String> missingStates = new ArrayList<>();
+        int missing = 0;
         for (NbtMap state : vanillaPalette) {
-            System.out.println("State does not exist in Cloudburst:\n" + state.toString());
-            failed = true;
+            missing++;
+            missingStates.add(state.toString());
         }
 
-        Assertions.assertFalse(failed, "One or more block states did not match the vanilla palette");
+        System.out.println("Found " + missing + " missing block states");
+        Files.write(Paths.get("missing_states.log"), missingStates, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+
+        Assertions.assertFalse(missing > 0 | invalid > 0, "One or more block states did not match the vanilla palette");
     }
 }
