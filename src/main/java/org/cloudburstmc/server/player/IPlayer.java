6e4ab40034cbe0af4055cb8c4b87753216165c72
Luke <32024335+lukeeey@users.noreply.github.com>
2020-09-23 01:02:44 +0100

    Change up the javadoc in IPlayer

--- src/main/java/org/cloudburstmc/server/player/IPlayer.java
+++ src/main/java/org/cloudburstmc/server/player/IPlayer.java
@@ -7,136 +7,98 @@ import org.cloudburstmc.server.permission.ServerOperator;
 import java.util.UUID;
 
 /**
- * 用来描述一个玩家和获得这个玩家相应信息的接口。<br>
- * An interface to describe a player and get its information.
- *
- * <p>这个玩家可以在线，也可以是不在线。<br>
- * This player can be online or offline.</p>
- *
+ * Represents the data for a player, whether they are online or offline.
+
  * @author MagicDroidX(code) @ Nukkit Project
  * @author 粉鞋大妈(javadoc) @ Nukkit Project
- * @see Player
- * @see OfflinePlayer
- * @since Nukkit 1.0 | Nukkit API 1.0.0
  */
 public interface IPlayer extends ServerOperator, Metadatable {
 
     /**
-     * 返回这个玩家是否在线。<br>
-     * Returns if this player is online.
+     * Checks if this player is currently online.
      *
-     * @return 这个玩家是否在线。<br>If this player is online.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return true if they are online
      */
     boolean isOnline();
 
     /**
-     * 返回这个玩家的名称。<br>
      * Returns the name of this player.
      *
-     * <p>如果是在线的玩家，这个函数只会返回登录名字。如果要返回显示的名字，参见{@link Player#getDisplayName}<br>
-     * Notice that this will only return its login name. If you need its display name, turn to
-     * {@link Player#getDisplayName}</p>
-     *
-     * @return 这个玩家的名称。<br>The name of this player.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return Player name
      */
     String getName();
 
     UUID getServerId();
 
     /**
-     * 返回这个玩家是否被封禁(ban)。<br>
-     * Returns if this player is banned.
+     * Checks if this player is banned.
      *
-     * @return 这个玩家的名称。<br>The name of this player.
-     * @see #setBanned
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return true if banned
      */
     boolean isBanned();
 
     /**
-     * 设置这个玩家是否被封禁(ban)。<br>
      * Sets this player to be banned or to be pardoned.
      *
-     * @param value 如果为{@code true}，封禁这个玩家。如果为{@code false}，解封这个玩家。<br>
-     *              {@code true} for ban and {@code false} for pardon.
-     * @see #isBanned
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @param value true if banned
      */
     void setBanned(boolean value);
 
     /**
-     * 返回这个玩家是否已加入白名单。<br>
-     * Returns if this player is pardoned by whitelist.
+     * Checks if this player is whitelisted.
      *
-     * @return 这个玩家是否已加入白名单。<br>If this player is pardoned by whitelist.
-     * @see Server#isWhitelisted
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return true if whitelisted
      */
     boolean isWhitelisted();
 
     /**
-     * 把这个玩家加入白名单，或者取消这个玩家的白名单。<br>
-     * Adds this player to the white list, or removes it from the whitelist.
+     * Adds or removes this player from the whitelist.
      *
-     * @param value 如果为{@code true}，把玩家加入白名单。如果为{@code false}，取消这个玩家的白名单。<br>
-     *              {@code true} for add and {@code false} for remove.
-     * @see #isWhitelisted
-     * @see Server#addWhitelist
-     * @see Server#removeWhitelist
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @param value true if whitelisted
      */
     void setWhitelisted(boolean value);
 
     /**
-     * 得到这个接口的{@code Player}对象。<br>
-     * Returns a {@code Player} object for this interface.
+     * Returns a {@link Player} object that this represents.
      *
-     * @return 这个接口的 {@code Player}对象。<br>a {@code Player} object for this interface.
-     * @see Server#getPlayerExact
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * If the player is online, this will return that player. Otherwise,
+     * it will return null.
+     *
+     * @return Player
      */
     Player getPlayer();
 
     /**
-     * 返回玩家所在的服务器。<br>
-     * Returns the server carrying this player.
+     * Returns the {@link Server} object carrying this player.
      *
-     * @return 玩家所在的服务器。<br>the server carrying this player.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return The server instance
      */
     Server getServer();
 
     /**
-     * 得到这个玩家第一次游戏的时间。<br>
-     * Returns the time this player first played in this server.
+     * Returns the time this player first played on this server.
      *
-     * @return 这个玩家第一次游戏的时间。<br>The time this player first played in this server.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * If the player has never played before, this will return 0. Otherwise, it
+     * will be the amount of milliseconds since midnight, January 1, 1970 UTC.
+     *
+     * @return The time this player first played on this server, or 0
      */
     Long getFirstPlayed();
 
     /**
-     * 得到这个玩家上次加入游戏的时间。<br>
      * Returns the time this player last joined in this server.
      *
-     * @return 这个玩家上次游戏的时间。<br>The time this player last joined in this server.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * If the player has never played before, this will return 0. Otherwise, it
+     * will be the amount of milliseconds since midnight, January 1, 1970 UTC.
+     *
+     * @return The time this player last played on this server, or 0
      */
     Long getLastPlayed();
 
     /**
-     * 返回这个玩家以前是否来过服务器。<br>
-     * Returns if this player has played in this server before.
+     * Checks if the player has played on this server before.
      *
-     * <p>如果想得到这个玩家是不是第一次玩，可以使用：<br>
-     * If you want to know if this player is the first time playing in this server, you can use:<br>
-     * </p>
-     * <pre>if(!player.hasPlayerBefore()) {...}</pre>
-     *
-     * @return 这个玩家以前是不是玩过游戏。<br>If this player has played in this server before.
-     * @since Nukkit 1.0 | Nukkit API 1.0.0
+     * @return true if this player has played before
      */
     boolean hasPlayedBefore();
 
