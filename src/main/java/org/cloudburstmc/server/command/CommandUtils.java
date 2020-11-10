b9d8d4e15e6fad843e04d356216f7f08cec6dff8
Creeperface01 <honzaxp01@gmail.com>
2020-11-10 12:16:47 +0100

    Implement setblock command and fix relative position (#102)

--- src/main/java/org/cloudburstmc/server/command/CommandUtils.java
+++ src/main/java/org/cloudburstmc/server/command/CommandUtils.java
@@ -18,7 +18,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 @UtilityClass
 public class CommandUtils {
-    private static final Pattern RELATIVE_PATTERN = Pattern.compile("(~)?(-?[0-9]*\\.?[0-9]*)");
+    private static final Pattern RELATIVE_PATTERN = Pattern.compile("(~)?([+\\-]?[0-9]+\\.?[0-9]*)?");
 
     public static Optional<Vector3f> parseVector3f(String[] args, Vector3f relative) {
         checkNotNull(args, "args");
@@ -41,7 +41,13 @@ public class CommandUtils {
     public static float getPosition(String pos, float relative) throws IllegalArgumentException {
         Matcher matcher = RELATIVE_PATTERN.matcher(pos);
         checkArgument(matcher.matches(), "Invalid position");
-        float position = Float.parseFloat(matcher.group(2));
+        float position;
+
+        if (matcher.group(2) != null) {
+            position = Float.parseFloat(matcher.group(2));
+        } else {
+            position = 0;
+        }
 
         if (matcher.group(1) != null) {
             position += relative;
