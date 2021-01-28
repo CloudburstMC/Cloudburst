4c2d524d335e200e618345e899939a9421679085
Sleepybear <sleepy@sleepybear.us>
2021-01-28 10:29:35 -0800

    Update Recipe stuff

--- api/src/main/java/org/cloudburstmc/api/registry/RecipeRegistry.java
+++ api/src/main/java/org/cloudburstmc/api/registry/RecipeRegistry.java
@@ -7,7 +7,7 @@ import org.cloudburstmc.api.util.Identifier;
 import java.nio.file.Path;
 import java.util.Collection;
 
-public interface RecipeRegistry {
+public interface RecipeRegistry extends Registry {
 
     void register(Identifier identifier, Recipe recipe) throws RegistryException;
 
@@ -16,14 +16,6 @@ public interface RecipeRegistry {
     Recipe getRecipe(Identifier identifier);
 
     Recipe matchRecipe(ItemStack[][] inputMap, ItemStack output, ItemStack[][] extraOutputMap);
-
-/*
-    FurnaceRecipe matchFurnaceRecipe(ItemStack input, Identifier craftingBlock);
-
-    BrewingRecipe matchBrewingRecipe(ItemStack input, ItemStack potion);
-
-    ContainerRecipe matchContainerRecipe(ItemStack input, ItemStack potion);
-*/
 
     Collection<Recipe> getRecipes();
 }
