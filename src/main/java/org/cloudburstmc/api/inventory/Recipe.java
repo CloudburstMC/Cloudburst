7a8ed8f9f8718f4d6de3909071519358d770a387
Sleepybear <sleepy@sleepybear.us>
2021-01-28 10:29:35 -0800

    Update Recipe stuff

--- src/main/java/org/cloudburstmc/api/inventory/Recipe.java
+++ src/main/java/org/cloudburstmc/api/inventory/Recipe.java
@@ -1,4 +1,14 @@
 package org.cloudburstmc.api.inventory;
 
+import org.cloudburstmc.api.item.ItemStack;
+import org.cloudburstmc.api.util.Identifier;
+
 public interface Recipe {
+    Identifier getId();
+
+    ItemStack getResult();
+
+    RecipeType getType();
+
+    Identifier getBlock();
 }
---
+++ src/main/java/org/cloudburstmc/api/inventory/RecipeType.java
@@ -0,0 +1,16 @@
+package org.cloudburstmc.api.inventory;
+
+public enum RecipeType {
+    SHAPELESS,
+    SHAPED,
+    FURNACE,
+    FURNACE_DATA,
+    MULTI,
+    SHULKER_BOX,
+    SHAPELESS_CHEMISTRY,
+    SHAPED_CHEMISTRY,
+
+    //Internal Use
+    POTION,
+    CONTAINER
+}
