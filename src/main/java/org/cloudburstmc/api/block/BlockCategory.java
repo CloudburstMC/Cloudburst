b51fffd3a775b97b034d399d737194e46778e6c8
Sleepybear <sleepy@sleepybear.us>
2021-01-29 18:47:39 -0800

    add block categories

---
+++ src/main/java/org/cloudburstmc/api/block/BlockCategory.java
@@ -0,0 +1,23 @@
+package org.cloudburstmc.api.block;
+
+public enum BlockCategory {
+    SOLID,
+    TRANSPARENT,
+    STAIRS,
+    SLAB,
+    DOUBLE_SLAB,
+    DOOR,
+    TRAPDOOR,
+    LOG,
+    LEAVES,
+    LIQUID,
+    CROPS,
+    FENCE,
+    FENCE_GATE,
+    WALLS,
+    SIGN,
+    STEM,
+    BUTTON,
+    PRESSURE_PLATE,
+    RAIL
+}
