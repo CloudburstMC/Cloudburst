db35619d093f20578755af3ba70114bf90748006
Sleepybear <sleepy@sleepybear.us>
2021-03-16 17:12:09 -0700

    move data classes and fixes

--- src/main/java/org/cloudburstmc/api/item/Food.java
+++ src/main/java/org/cloudburstmc/api/item/Food.java
@@ -1,4 +1,8 @@
 package org.cloudburstmc.api.item;
 
+import org.cloudburstmc.api.player.Player;
+
 public interface Food {
+
+    boolean onEatenBy(Player player);
 }
---
+++ src/main/java/org/cloudburstmc/api/level/chunk/ChunkException.java
@@ -0,0 +1,21 @@
+package org.cloudburstmc.api.level.chunk;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class ChunkException extends RuntimeException {
+
+    public ChunkException(String message) {
+        super(message);
+    }
+
+    public ChunkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ChunkException(Throwable cause) {
+        super(cause);
+    }
+
+}
