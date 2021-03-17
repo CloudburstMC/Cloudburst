0ab56b4ea14b443c5ed2583993a19a8763644124
Sleepybear <sleepy@sleepybear.us>
2021-03-17 09:44:23 -0700

    block entity and inventory fixes

--- api/src/main/java/org/cloudburstmc/api/inventory/FurnaceInventory.java
+++ api/src/main/java/org/cloudburstmc/api/inventory/FurnaceInventory.java
@@ -1,9 +1,22 @@
 package org.cloudburstmc.api.inventory;
 
 import org.cloudburstmc.api.blockentity.Furnace;
+import org.cloudburstmc.api.item.ItemStack;
 
 public interface FurnaceInventory extends Inventory {
 
     @Override
     Furnace getHolder();
+
+    ItemStack getResult();
+
+    boolean setResult(ItemStack item);
+
+    ItemStack getFuel();
+
+    boolean setFuel(ItemStack item);
+
+    ItemStack getSmelting();
+
+    boolean setSmelting(ItemStack item);
 }
