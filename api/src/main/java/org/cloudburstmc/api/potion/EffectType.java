0c7b3b00b9ab13cc5cfd74990a35e4f9d262ae38
Sleepybear <sleepy@sleepybear.us>
2021-03-25 17:20:58 -0700

    Refactor Potion/Effect types to remove network data from API

--- api/src/main/java/org/cloudburstmc/api/potion/EffectType.java
+++ api/src/main/java/org/cloudburstmc/api/potion/EffectType.java
@@ -1,7 +1,5 @@
 package org.cloudburstmc.api.potion;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import com.nukkitx.math.vector.Vector3i;
 import lombok.Getter;
 import org.cloudburstmc.api.util.Identifier;
@@ -11,39 +9,25 @@ import java.util.HashMap;
 
 @Getter
 public class EffectType {
-    private final byte networkId;
     private final Identifier id;
     private final Vector3i color;
     private final boolean bad;
-    private static final BiMap<Identifier, Byte> netIdMap = HashBiMap.create();
     private static final HashMap<Identifier, EffectType> effectMap = new HashMap<>();
 
-    EffectType(Identifier id, byte networkId, Vector3i color) {
-        this(id, networkId, color, false);
+    EffectType(Identifier id, Vector3i color) {
+        this(id, color, false);
     }
 
-    EffectType(Identifier id, byte networkId, Vector3i color, boolean isBad) {
+    EffectType(Identifier id, Vector3i color, boolean isBad) {
         this.id = id;
-        this.networkId = networkId;
         this.color = color;
         this.bad = isBad;
 
-        netIdMap.put(id, networkId);
         effectMap.put(id, this);
-    }
-
-    public static EffectType fromLegacy(byte id) {
-        return effectMap.get(netIdMap.inverse().get(id));
     }
 
     @Nullable
     public static EffectType byName(String arg) {
-        Identifier id = Identifier.fromString(arg);
-
-        for(EffectType type : effectMap.values()) {
-            if(type.getId().equals(id))
-                return type;
-        }
-        return null;
+        return effectMap.get(Identifier.fromString(arg));
     }
 }
--- api/src/main/java/org/cloudburstmc/api/potion/PotionType.java
+++ api/src/main/java/org/cloudburstmc/api/potion/PotionType.java
@@ -1,7 +1,5 @@
 package org.cloudburstmc.api.potion;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
@@ -15,12 +13,10 @@ import java.util.HashMap;
 @Builder
 public class PotionType {
 
-    private static final BiMap<Identifier, Byte> netIdMap = HashBiMap.create();
     private static final HashMap<Identifier, PotionType> typeMap = new HashMap<>();
 
     @Nonnull
     private Identifier potionId;
-    private final int networkId;
     private final EffectType type;
     private final int level;
     private final int duration;
@@ -28,25 +24,18 @@ public class PotionType {
     @Setter
     private boolean splash;
 
-    public PotionType(Identifier id, int netId, EffectType type, int level, int duration, boolean instant) {
-        this(id, netId, type, level, duration, instant, false);
+    public PotionType(Identifier id, EffectType type, int level, int duration, boolean instant) {
+        this(id, type, level, duration, instant, false);
     }
 
-    public PotionType(Identifier id, int netId, EffectType type, int level, int duration, boolean instant, boolean splash) {
-        this.networkId= netId;
+    public PotionType(Identifier id, EffectType type, int level, int duration, boolean instant, boolean splash) {
         this.type = type;
         this.level = level;
         this.duration = duration;
         this.instant = instant;
         this.splash = splash;
 
-        netIdMap.put(id, (byte) netId);
         typeMap.put(id, this);
-    }
-
-    @Nullable
-    public static PotionType fromLegacy(byte networkId) {
-        return typeMap.get(netIdMap.inverse().get(networkId));
     }
 
     @Nullable
