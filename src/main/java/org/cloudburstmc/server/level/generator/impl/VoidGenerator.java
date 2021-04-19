c9a995d9e4c39cd440d13450b90287e548029dc8
rtm516 <rtm516@users.noreply.github.com>
2021-04-19 11:24:15 +0100

    Remove platform from VoidGenerator by default (#115)

--- src/main/java/org/cloudburstmc/server/level/generator/impl/VoidGenerator.java
+++ src/main/java/org/cloudburstmc/server/level/generator/impl/VoidGenerator.java
@@ -15,18 +15,22 @@ import org.cloudburstmc.server.utils.Identifier;
 public final class VoidGenerator implements Generator {
     public static final Identifier ID = Identifier.from("minecraft", "void");
 
+    private final boolean enablePlatform;
+
     public VoidGenerator(long seed, String options) {
-        //no-op
+        this.enablePlatform = options.equals("enablePlatform");
     }
 
     @Override
     public void generate(PRandom random, IChunk chunk, int chunkX, int chunkZ) {
-        int i = chunkX | chunkZ;
-        if (((i | (i >> 31)) & ~1) == 0) {
-            //both chunk coordinates are either 0 or 1
-            for (int x = 0; x < 16; x++) {
-                for (int z = 0; z < 16; z++) {
-                    chunk.setBlock(x, 64, z, 0, BlockStates.STONE);
+        if (enablePlatform) {
+            int i = chunkX | chunkZ;
+            if (((i | (i >> 31)) & ~1) == 0) {
+                //both chunk coordinates are either 0 or 1
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        chunk.setBlock(x, 64, z, 0, BlockStates.STONE);
+                    }
                 }
             }
         }
