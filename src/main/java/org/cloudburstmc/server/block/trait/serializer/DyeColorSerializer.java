7c3ffbc0a10d86bf2838ed40d472199957ffea2b
Sleepybear <sleepy@sleepybear.us>
2021-08-21 11:39:45 -0700

    Add Candles & Candle Cakes Refactor Color traits

--- src/main/java/org/cloudburstmc/server/block/trait/serializer/DyeColorSerializer.java
+++ src/main/java/org/cloudburstmc/server/block/trait/serializer/DyeColorSerializer.java
@@ -2,6 +2,7 @@ package org.cloudburstmc.server.block.trait.serializer;
 
 import com.nukkitx.nbt.NbtMapBuilder;
 import org.cloudburstmc.api.block.BlockType;
+import org.cloudburstmc.api.block.BlockTypes;
 import org.cloudburstmc.api.block.trait.BlockTrait;
 import org.cloudburstmc.api.util.data.DyeColor;
 import org.cloudburstmc.server.block.trait.BlockTraitSerializers.TraitSerializer;
@@ -14,7 +15,8 @@ public class DyeColorSerializer implements TraitSerializer<DyeColor> {
 
     @Override
     public Comparable<?> serialize(NbtMapBuilder builder, BlockType type, Map<BlockTrait<?>, Comparable<?>> traits, DyeColor color) {
-        if (color == DyeColor.LIGHT_GRAY) {
+        if (color == DyeColor.LIGHT_GRAY &&
+                (type != BlockTypes.CANDLE && type != BlockTypes.CANDLE_CAKE)) {
             return "silver";
         }
         return null;
