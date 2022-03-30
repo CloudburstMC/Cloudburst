6d2db550cbc7ba5ca891ef6d1f44df511a970029
Kazuk <46299532+Kazzuk@users.noreply.github.com>
2022-03-30 16:45:59 +0100

    Add 1.18.10 support

--- src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
+++ src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
@@ -1,7 +1,7 @@
 package org.cloudburstmc.server.network;
 
 import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
-import com.nukkitx.protocol.bedrock.v475.Bedrock_v475;
+import com.nukkitx.protocol.bedrock.v486.Bedrock_v486;
 import lombok.extern.log4j.Log4j2;
 
 import javax.annotation.Nonnegative;
@@ -22,7 +22,7 @@ public final class ProtocolInfo {
     private static BedrockPacketCodec DEFAULT_PACKET_CODEC;
 
     static {
-        setDefaultPacketCodec(Bedrock_v475.V475_CODEC);
+        setDefaultPacketCodec(Bedrock_v486.V486_CODEC);
     }
 
     public static BedrockPacketCodec getDefaultPacketCodec() {
