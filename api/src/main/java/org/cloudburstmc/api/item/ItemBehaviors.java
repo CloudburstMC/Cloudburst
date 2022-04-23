9201491a74cf509e6fc40d4b9ddec8a50b15e2d5
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2022-04-23 21:04:18 +0100

    Add CAN_DESTROY item behaviour

--- api/src/main/java/org/cloudburstmc/api/item/ItemBehaviors.java
+++ api/src/main/java/org/cloudburstmc/api/item/ItemBehaviors.java
@@ -1,6 +1,7 @@
 package org.cloudburstmc.api.item;
 
 import lombok.experimental.UtilityClass;
+import org.cloudburstmc.api.block.behavior.BooleanBlockBehavior;
 import org.cloudburstmc.api.data.BehaviorKey;
 import org.cloudburstmc.api.data.DataKey;
 import org.cloudburstmc.api.util.Identifier;
@@ -12,4 +13,6 @@ public class ItemBehaviors {
     public static final BehaviorKey<IntBehavior, IntBehavior.Executor> GET_MAX_STACK_SIZE = DataKey.behavior(Identifier.fromString("get_max_stack_size"), IntBehavior.class, IntBehavior.Executor.class);
 
     public static final BehaviorKey<IntBehavior, IntBehavior.Executor> GET_MAX_DURABILITY = DataKey.behavior(Identifier.fromString("get_max_durability"), IntBehavior.class, IntBehavior.Executor.class);
+
+    public static final BehaviorKey<BooleanBlockBehavior, BooleanBlockBehavior.Executor> CAN_DESTROY = DataKey.behavior(Identifier.fromString("can_destroy"), BooleanBlockBehavior.class, BooleanBlockBehavior.Executor.class);
 }
