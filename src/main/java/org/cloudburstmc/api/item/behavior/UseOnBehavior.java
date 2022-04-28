de67eae87bfc580491fd611c26f265215244aab1
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2022-04-28 22:50:25 +0100

    Expand available item behaviours

---
+++ src/main/java/org/cloudburstmc/api/item/behavior/UseOnBehavior.java
@@ -0,0 +1,19 @@
+package org.cloudburstmc.api.item.behavior;
+
+import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.math.vector.Vector3i;
+import org.cloudburstmc.api.entity.Entity;
+import org.cloudburstmc.api.item.ItemStack;
+import org.cloudburstmc.api.util.Direction;
+import org.cloudburstmc.api.util.behavior.Behavior;
+
+public interface UseOnBehavior {
+
+    boolean useOn(Behavior<Executor> behavior, ItemStack itemStack, Entity entity, Vector3i blockPosition, Direction face, Vector3f clickPosition);
+
+    @FunctionalInterface
+    interface Executor {
+
+        boolean execute(ItemStack itemStack, Entity entity, Vector3i blockPosition, Direction face, Vector3f clickPosition);
+    }
+}
