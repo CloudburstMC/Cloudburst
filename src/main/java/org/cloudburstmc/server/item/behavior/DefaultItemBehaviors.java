7f00045a3ed1edae0636e9ee8eb45505263344b1
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2022-04-28 23:38:03 +0100

    Implement default damage behaviour

--- src/main/java/org/cloudburstmc/server/item/behavior/DefaultItemBehaviors.java
+++ src/main/java/org/cloudburstmc/server/item/behavior/DefaultItemBehaviors.java
@@ -1,14 +1,18 @@
 package org.cloudburstmc.server.item.behavior;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.cloudburstmc.api.block.BlockBehaviors;
+import org.cloudburstmc.api.block.BlockType;
 import org.cloudburstmc.api.enchantment.Enchantment;
 import org.cloudburstmc.api.enchantment.EnchantmentTypes;
+import org.cloudburstmc.api.item.ItemBehaviors;
 import org.cloudburstmc.api.item.ItemKeys;
-import org.cloudburstmc.api.item.behavior.DamageChanceBehavior;
-import org.cloudburstmc.api.item.behavior.FloatItemBehavior;
-import org.cloudburstmc.api.item.behavior.DestroySpeedBehavior;
+import org.cloudburstmc.api.item.ItemStack;
+import org.cloudburstmc.api.item.behavior.*;
 import org.cloudburstmc.server.item.ItemUtils;
 
+@Slf4j
 @UtilityClass
 public class DefaultItemBehaviors {
 
@@ -28,4 +32,46 @@ public class DefaultItemBehaviors {
     public static final Boolean CAN_DESTROY_IN_CREATIVE = true;
 
     public static final DamageChanceBehavior GET_DAMAGE_CHANCE = (executor, unbreaking) -> 100 / (unbreaking + 1);
+
+    public static final Float GET_FUEL_DURATION = 0.0F;
+
+    public static final MineBlockBehavior MINE_BLOCK = (behavior, itemStack, block, owner) -> {
+        if (block.getBehaviors().get(BlockBehaviors.CAN_DAMAGE_ITEM)) {
+            return behavior.get(ItemBehaviors.ON_DAMAGE).execute(itemStack, 2, owner);
+        }
+        return itemStack;
+    };
+
+    public static final DamageItemBehavior ON_DAMAGE = (behavior, itemStack, damage, owner) -> {
+        if (damage <= 0 || ItemUtils.isNull(itemStack) || behavior.get(ItemBehaviors.GET_MAX_DURABILITY).execute() < 0 ||
+                !owner.isAlive() || itemStack.get(ItemKeys.UNBREAKABLE) != Boolean.TRUE) {
+            if (damage < 0) {
+                log.debug("Tried to damage {} with a negative value of {}", itemStack, damage);
+            }
+            return itemStack;
+        }
+
+        Enchantment enchantment = itemStack.get(ItemKeys.ENCHANTMENTS).get(EnchantmentTypes.UNBREAKING);
+        int enchantmentLevel = enchantment == null ? 0 : enchantment.level();
+
+        int damageChance = behavior.get(ItemBehaviors.GET_DAMAGE_CHANCE).execute(enchantmentLevel);
+
+        if (enchantmentLevel > 0 && damageChance <= 100) {
+            return itemStack;
+        }
+
+        Integer damageKey = itemStack.get(ItemKeys.DAMAGE);
+        int damageValue = damageKey == null ? 0 : damageKey;
+
+        damageValue += damage;
+
+        if (damageValue >= behavior.get(ItemBehaviors.GET_MAX_DURABILITY).execute()) {
+            // TODO: Make the break sound
+            return ItemStack.AIR;
+        }
+
+        return itemStack.toBuilder()
+                .data(ItemKeys.DAMAGE, damageValue)
+                .build();
+    };
 }
