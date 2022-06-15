9b834218050885f696de5ae975f6f0b2940f321e
Creeperface01 <honzaxp01@gmail.com>
2022-06-15 21:20:21 +0900

    Refactor block placing (#160)
    * Refactor block placing
    
    * Fix build
    
    * Register all vanilla block and item behaviors
    
    * Fix default value
    
    Co-authored-by: Jan Bednář <jan.bednar@lemenio.com>

---
+++ src/main/java/org/cloudburstmc/server/block/behavior/defaults/PlaceBlockBehavior.java
@@ -0,0 +1,13 @@
+package org.cloudburstmc.server.block.behavior.defaults;
+
+import org.cloudburstmc.api.block.Block;
+import org.cloudburstmc.api.block.behavior.ComplexBlockBehavior;
+import org.cloudburstmc.api.util.behavior.Behavior;
+
+public class PlaceBlockBehavior implements ComplexBlockBehavior {
+
+    @Override
+    public void execute(Behavior<Executor> behavior, Block block) {
+
+    }
+}
