61b276158f52868f3a8a42d12f45b3583f5f7014
Kanelucky <tuankietdoan46@gmail.com>
2026-08-16 21:07:39 +0700

    feat: 26.44 support

--- server/src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
+++ server/src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
@@ -3,7 +3,7 @@ package org.cloudburstmc.server.network;
 import lombok.extern.log4j.Log4j2;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v2168.Bedrock_v2168;
+import org.cloudburstmc.protocol.bedrock.codec.v2168.Bedrock_v2168_hotfix4;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -22,7 +22,7 @@ public final class ProtocolInfo {
     private static BedrockCodec DEFAULT_PACKET_CODEC;
 
     static {
-        setDefaultPacketCodec(Bedrock_v2168.CODEC);
+        setDefaultPacketCodec(Bedrock_v2168_hotfix4.CODEC);
     }
 
     public static BedrockCodec getDefaultPacketCodec() {
