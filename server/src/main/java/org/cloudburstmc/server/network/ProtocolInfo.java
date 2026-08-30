626d6e507414d645fccfa0ba142118a92f20e45f
Kazuk <46299532+Kazzuk@users.noreply.github.com>
2026-08-30 13:46:16 +0100

    Add support for 1.26.45 (2169)

--- server/src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
+++ server/src/main/java/org/cloudburstmc/server/network/ProtocolInfo.java
@@ -3,7 +3,7 @@ package org.cloudburstmc.server.network;
 import lombok.extern.log4j.Log4j2;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v2168.Bedrock_v2168_hotfix4;
+import org.cloudburstmc.protocol.bedrock.codec.v2169.Bedrock_v2169;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -22,7 +22,7 @@ public final class ProtocolInfo {
     private static BedrockCodec DEFAULT_PACKET_CODEC;
 
     static {
-        setDefaultPacketCodec(Bedrock_v2168_hotfix4.CODEC);
+        setDefaultPacketCodec(Bedrock_v2169.CODEC);
     }
 
     public static BedrockCodec getDefaultPacketCodec() {
